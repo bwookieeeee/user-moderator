da797561ff39badb2aebfe768305f87570d2282f
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2022-01-17 11:51:27 -0800

    add mass ban slash command w basic option handler

---
+++ src/commands/massban.js
@@ -0,0 +1,43 @@
+/*  Massban - Sends a ban request for every banned username and/or IP address
+ *  tracked in the user mod database. This is particularly useful for when remo
+ *  falls out of sync, like if it were to restart.
+ */
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('massban')
+    .setDescription('Sends a ban request to remo for every known banned username and IP address')
+    .addStringOption(option =>
+      option.setName("target")
+        .setDescription("Select users or IPs or all to ban. Default all.")
+        .setRequired(false)
+        .addChoice("users", "users")
+        .addChoice("ips", "ips")
+        .addChoice("all", "all")),
+  async execute(interaction) {
+    await interaction.deferReply();
+    const target = interaction.options.getString("target", false) ? interaction.options.getString("target") : "all";
+    console.log(`mass ban initiated by ${interaction.user.username} w opts { ${target} }`);
+
+    // eslint-disable-next-line prefer-const
+    let toBan = [];
+    if (target === "users" || target === "all") {
+      // Pull all banned users from db
+      toBan.push();
+    }
+    if (target === "ips" || target === "all") {
+      // Pull all banned IPs from db.
+      toBan.push();
+    }
+
+    try {
+      await interaction.followUp(`This isn't implemented yet; but if it were, ${toBan.length} bans would've been sent.`);
+      // this is where you'd do the banning if that were implemented yet.
+    }
+    catch (e) {
+      await interaction.followUp("Couldn't complete request, please check logs.");
+    }
+  },
+};

7e871310b219e94e4c4acefdf466f7dbec383533
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2022-01-18 11:18:27 -0800

    set reply to ephemeral

--- src/commands/massban.js
+++ src/commands/massban.js
@@ -17,11 +17,10 @@ module.exports = {
         .addChoice("ips", "ips")
         .addChoice("all", "all")),
   async execute(interaction) {
-    await interaction.deferReply();
+    await interaction.deferReply({ ephemeral: true });
     const target = interaction.options.getString("target", false) ? interaction.options.getString("target") : "all";
     console.log(`mass ban initiated by ${interaction.user.username} w opts { ${target} }`);
 
-    // eslint-disable-next-line prefer-const
     let toBan = [];
     if (target === "users" || target === "all") {
       // Pull all banned users from db
@@ -33,11 +32,11 @@ module.exports = {
     }
 
     try {
-      await interaction.followUp(`This isn't implemented yet; but if it were, ${toBan.length} bans would've been sent.`);
+      await interaction.editReply(`This isn't implemented yet; but if it were, ${toBan.length} bans would've been sent.`);
       // this is where you'd do the banning if that were implemented yet.
     }
     catch (e) {
-      await interaction.followUp("Couldn't complete request, please check logs.");
+      await interaction.editReply("Couldn't complete request, please check logs.");
     }
   },
 };

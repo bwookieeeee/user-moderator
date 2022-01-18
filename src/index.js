00378256fa5ab3d3f193fdcaba125cc6688b41f9
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2022-01-17 16:20:55 -0800

    report user authentication events to discord

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ copyright (R) 2022 Brooke Morrison ALL RIGHTS RESERVED
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
 
 const fs = require("fs");
-const { Client, Collection, Intents } = require('discord.js');
-const { token, eventChannel, logChannel } = require('./config.json').discord;
-const { key, url } = require("./config.json").remo;
+const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
+const { token, eventChannelId, logChannelId, testChannelId } = require('./config.json').discord;
+const { ws, remoevts } = require("./remo");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -20,8 +20,16 @@ for (const file of cmdFiles) {
   client.commands.set(cmd.data.name, cmd);
 }
 
-client.once('ready', () => {
+let eventChannel;
+let logChannel;
+let testChannel;
+
+client.once('ready', async () => {
   console.log('Ready');
+  ws.login();
+  eventChannel = await client.channels.fetch(eventChannelId);
+  logChannel = await client.channels.fetch(logChannelId);
+  testChannel = await client.channels.fetch(testChannelId);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -40,4 +48,23 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply({ content: "There was an error while executing this command", ephemeral: true });
   }
 });
+
+// Report when a user logs in
+remoevts.on("NEW_LOGIN", data => {
+  const { username, cores, gpu, userAgent, ip, countryCode, isp, proxy, usernameBanned, ipBanned, width, height, time } = data;
+
+  const embed = new MessageEmbed()
+    .setColor("#9DFF00")
+    .setTitle(username)
+    .setURL("https://remo.tv")
+    .setDescription(`**Cores:** ${cores}\n**GPU:** ${gpu}`
+      + `\n**IP Address:** ${ip}\n**User-Agent:** ${userAgent}\n**Country Code:** ${countryCode}`
+      + `\n**ISP:** ${isp}\n**Proxy:** ${proxy}\n**Username Banned:** ${usernameBanned}`
+      + `\n**IP Banned:** ${ipBanned}\n**Width:** ${width}\n**Height:** ${height}`)
+    .setTimestamp();
+
+  testChannel.send({ embeds: [embed] });
+});
+
+
 client.login(token);

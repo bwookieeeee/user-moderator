3520e34f13b34a173e79697e03ba9ee96225bb71
Brooke Morrison <5741965+bwookieeeee@users.noreply.github.com>
2022-01-19 17:07:15 -0800

    Add getAllBannedUsers, and fix a few bugs I didn't catch

--- src/database.js
+++ src/database.js
@@ -100,16 +100,17 @@ module.exports.createIp = async ip => {
 
 module.exports.updateUser = async user => {
   const existingUser = await this.getUser(user.username);
-  const banned = user.banned || existingUser.banned;
+  const banned = user.banned !== undefined ? user.banned : existingUser.banned;
   const ips = user.ips || existingUser.ips;
-  const prejudiced = user.prejudiced || existingUser.prejudiced;
+  const prejudiced =
+    user.prejudiced !== undefined ? user.prejudiced : existingUser.prejudiced;
   console.log(
     `update user ${user.username} w opts { ${banned}, ${prejudiced}, ${ips} }`
   );
 
   try {
     await pool.query(
-      "UPDATE TABLE users SET (ips, banned, prejudiced) VALUES ($1,$2,$3) WHERE username=$4",
+      "UPDATE users SET ips=$1, banned=$2, prejudiced=$3 WHERE username=$4",
       [ips, banned, prejudiced, user.username]
     );
     return {
@@ -128,10 +129,11 @@ module.exports.updateUser = async user => {
 module.exports.updateIp = async ip => {
   console.log(`update ip ${ip.addr} w opts { ${ip.banned}, ${ip.prejudiced} }`);
   try {
-    await pool.query(
-      "UPDATE TABLE ips SET (banned, prejudiced) VALUES ($1,$2) WHERE ip=$3",
-      [ip.banned, ip.prejudiced, ip.addr]
-    );
+    await pool.query("UPDATE ips SET banned=$1, prejudiced=$2 WHERE ip=$3", [
+      ip.banned,
+      ip.prejudiced,
+      ip.addr
+    ]);
     return ip;
   } catch (e) {
     console.error(e);
@@ -141,9 +143,31 @@ module.exports.updateIp = async ip => {
 
 module.exports.queryAlts = async addr => {
   try {
-    const res = await pool.query("SELCT * FROM users WHERE $1=ANY(ips)", [
+    const res = await pool.query("SELECT * FROM users WHERE $1=ANY(ips)", [
       addr
     ]);
+    return res.rows;
+  } catch (e) {
+    console.error(e);
+    return { err: e };
+  }
+};
+
+module.exports.getAllBannedUsers = async () => {
+  try {
+    const res = await pool.query(
+      "SELECT username FROM users WHERE banned=true"
+    );
+    return res.rows;
+  } catch (e) {
+    console.error(e);
+    return { err: e };
+  }
+};
+
+module.exports.getAllBannedIps = async () => {
+  try {
+    const res = await pool.query("SELECT ip FROM ips WHERE banned=true");
     return res.rows;
   } catch (e) {
     console.error(e);
